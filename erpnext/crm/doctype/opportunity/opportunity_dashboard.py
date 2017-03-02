87847b1789163824547d3c6e8bcd27fc26e5941d
riversoul <antoniodlpc@gmail.com>
2017-03-02 16:09:23 -0500

    Opportunity Dashboard
    Ajout de resquest for quotation dans les related documents de
    opportunity. Doit egalement ajouter custom field dans RFQ.

--- erpnext/crm/doctype/opportunity/opportunity_dashboard.py
+++ erpnext/crm/doctype/opportunity/opportunity_dashboard.py
@@ -1,14 +1,16 @@
 from frappe import _
 
 def get_data():
+	# 2017-01-19 - RM - Ajout de resquest for quotation dans les related documents de opportunity. Doit egalement ajouter custom field dans RFQ.
 	return {
 		'fieldname': 'prevdoc_docname',
 		'non_standard_fieldnames': {
 			'Supplier Quotation': 'opportunity',
+			'Request for Quotation': 'opportunity',
 		},
 		'transactions': [
 			{
-				'items': ['Quotation', 'Supplier Quotation']
+				'items': ['Quotation', 'Supplier Quotation', 'Request for Quotation']
 			},
 		]
 	}
